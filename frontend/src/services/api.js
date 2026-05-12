6d7eeca4202aefde386f2abe9df8b3b606c908f9
Pranshu Koshta <pranshukoshta7@gmail.com>
2026-05-12 10:04:26 +0530

    built api connecting frontend to backend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+export async function evaluateAnswers(data) {
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/evaluate",
+    {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify(data)
+    }
+  );
+
+  return await response.json();
+}
